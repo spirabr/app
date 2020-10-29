27c2111bb96d6f4cec8635363c5010ec9833b92c
thlasso <thaislasso@gmail.com>
2020-10-29 17:55:05 -0300

    feat: create component novaColeta
    Signed-off-by: Alexia Scheffer <ac.scheffer@usp.br>
    Signed-off-by: Matheus Santos <mattsantos.conce@gmail.com>
    Signed-off-by: Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
    Signed-off-by: Ricardo Kojo <ricardokojo7@gmail.com>
    Signed-off-by: Thais Lasso <thlasso@usp.br>
    Signed-off-by: Vinicius Pereira <vini.gpereira2014@gmail.com>
    Signed-off-by: Wander Souza <wanderdasouza@gmail.com>

---
+++ lib/novaColeta.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NovaColeta extends StatefulWidget {
+  NovaColeta(String dropdownValue);
+  @override
+  _NovaColetaState createState() => _NovaColetaState();
+}
+
+class _NovaColetaState extends State<NovaColeta> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
