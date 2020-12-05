a480b7606843a166ed16303e9105d98391caba66
victorhmp <victor2142@gmail.com>
2020-12-05 19:18:54 -0300

    chore:replace audio_recorder with flutter_sound library
    Co-authored-by: Alexia Scheffer <ac.scheffer@usp.br>
    Co-authored-by: Matheus Santos <mattsantos.conce@gmail.com>
    Co-authored-by: Victor Miranda <victor2142@gmail.com>
    Co-authored-by: Wander Souza <wanderdasouza@gmail.com>

--- lib/gravacao.dart
+++ lib/gravacao.dart
@@ -1,47 +1,72 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:audio_recorder/audio_recorder.dart';
-import 'package:file/local.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_sound/flutter_sound.dart';
 import 'package:stop_watch_timer/stop_watch_timer.dart';
 
 class Gravacao extends StatefulWidget {
-  final LocalFileSystem localFileSystem;
-
-  Gravacao({localFileSystem})
-      : this.localFileSystem = localFileSystem ?? LocalFileSystem();
-
   @override
   _GravacaoState createState() => _GravacaoState();
 }
 
 class _GravacaoState extends State<Gravacao> {
   final StopWatchTimer _stopWatchTimer = StopWatchTimer();
+  final FlutterSoundRecorder _mRecorder = FlutterSoundRecorder();
+  String _mPath;
+  bool _mRecorderStarted;
 
-  Recording _recording = Recording();
   bool _isRecording = false;
+
+  @override
+  void initState() {
+    openTheRecorder().then((value) {
+      setState(() {
+        _mRecorderStarted = true;
+      });
+    });
+    super.initState();
+  }
+
+  Future<void> openTheRecorder() async {
+    var tempDir = await getApplicationDocumentsDirectory();
+    _mPath = '${tempDir.path}/flutter_sound_example.aac';
+    var outputFile = File(_mPath);
+
+    if (outputFile.existsSync()) {
+      await outputFile.delete();
+    }
+
+    await _mRecorder.openAudioSession();
+
+    _mRecorderStarted = true;
+  }
 
   void _start() async {
     try {
-      if (await AudioRecorder.hasPermissions) {
-        await AudioRecorder.start();
-        bool isRecordingAux = await AudioRecorder.isRecording;
-        setState(() {
-          _recording = Recording(duration: Duration(), path: "");
-          _isRecording = isRecordingAux;
-        });
-      } else {
-        //Scaffold.of(context).showSnackBar(
-         //   SnackBar(content: Text("You must accept permissions")));
-      }
+      assert(_mRecorderStarted);
+
+      await _mRecorder.startRecorder(
+        toFile: _mPath,
+        codec: Codec.aacADTS,
+      );
+
+      var isRecordingAux = _mRecorder.isRecording;
+
+      setState(() {
+        _isRecording = isRecordingAux;
+      });
+      // ignore: avoid_catches_without_on_clauses
     } catch (e) {
       print(e);
     }
   }
 
   void _stop() async {
-    var recording = await AudioRecorder.stop();
-    bool isRecording = await AudioRecorder.isRecording;
+    await _mRecorder.stopRecorder();
+
+    var isRecording = _mRecorder.isRecording;
+
     setState(() {
-      _recording = recording;
       _isRecording = isRecording;
     });
   }
@@ -61,30 +86,36 @@ class _GravacaoState extends State<Gravacao> {
                 initialData: 0,
                 builder: (context, snap) {
                   final time = snap.data;
-                  return Text(StopWatchTimer.getDisplayTime(time, hours: false, minute: false), style: TextStyle(
-                    fontSize: 38
-                  ),);
+                  return Text(
+                    StopWatchTimer.getDisplayTime(time,
+                        hours: false, minute: false),
+                    style: TextStyle(fontSize: 38),
+                  );
                 }),
             GestureDetector(
-                onLongPressStart: (_) {
-                  _stopWatchTimer.onExecute.add(StopWatchExecute.start);
-                  _start();
-                },
-                onLongPressEnd: (_) {
-                  _stopWatchTimer.onExecute.add(StopWatchExecute.stop);
-                  _stop();
-                  _stopWatchTimer.onExecute.add(StopWatchExecute.reset);
-                },
-                child: Container(
-                  padding: EdgeInsets.all(30),
-                  child: Icon(
-                    Icons.mic,
-                    color: Colors.white,
-                  ),
-                  decoration: BoxDecoration(
-                      color: _isRecording ? Colors.grey : Theme.of(context).primaryColor,
-                      shape: BoxShape.circle),
-                ))
+              onLongPressStart: (_) {
+                _stopWatchTimer.onExecute.add(StopWatchExecute.start);
+                _start();
+              },
+              onLongPressEnd: (_) {
+                _stopWatchTimer.onExecute.add(StopWatchExecute.stop);
+                _stop();
+                _stopWatchTimer.onExecute.add(StopWatchExecute.reset);
+              },
+              child: Container(
+                padding: EdgeInsets.all(30),
+                child: Icon(
+                  Icons.mic,
+                  color: Colors.white,
+                ),
+                decoration: BoxDecoration(
+                  color: _isRecording
+                      ? Colors.grey
+                      : Theme.of(context).primaryColor,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            )
           ],
         ),
       ),
